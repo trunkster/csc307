3db26eab0081b3b82328b82bd106956b8af90ce6
Damien Trunkey <dtrunkey@calpoly.edu>
2021-04-14 15:19:46 -0700

    made change

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,6 @@ public class printing
 {
 	public static void main(String[] args)
 	{
-		System.out.print("I've been at cal poly for 3 years");
 		System.out.print("Damien Trunkey made a change");
 	}
 }
